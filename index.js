e44692e361a68df6688734f554e4b05052d3fa3c
Denis <51417134+SinedGG@users.noreply.github.com>
2023-08-20 17:20:55 +0300

    🍃 | New timer

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
-const schedule = require("node-schedule");
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.TG_TOKEN);
 
 const backup = require("./modules/dbBackup");
-const rule = new schedule.RecurrenceRule();
-rule.hour = [0, 6, 12, 18];
-schedule.scheduleJob(rule, () => {
+
+backup(bot);
+setInterval(() => {
   backup(bot);
-});
+}, 6 * 60 * 60 * 1000);
 
 require("./modules/errLog")(bot);
 
